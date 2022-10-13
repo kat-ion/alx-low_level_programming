d65ebc168c7f232d2c4a16db09e6982d36134157
kat-ion <katz108244@gmail.com>
2022-10-13 05:30:04 +0200

    updates

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
  * print_name - f points to either func in main
  * @name: name
--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -19,5 +19,4 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-
 #endif /* CALC_H */
