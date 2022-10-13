36fc65a54a1831667da9a5d26d5818f231f09c35
kat-ion <katz108244@gmail.com>
2022-10-13 05:41:19 +0200

    x

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -27,13 +27,13 @@ void print_name_uppercase(char *name)
     while (name[i])
     {
         if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
+            {
+                putchar(name[i] + 'A' - 'a');
+            }
         else
-        {
-            putchar(name[i]);
-        }
+            {
+                putchar(name[i]);
+            }
         i++;
     }
 }
