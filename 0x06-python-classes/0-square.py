1607cc6e5f9edd18ae04a8d54e654c2c2bb3b8cf
kat-ion <katz108244@gmail.com>
2022-12-21 00:52:13 +0200

    Init Commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""An epmty class to contain our work."""
+
+class square():
+    """Create class square
+
+"""
+    pass
+"""Class to be passed for now. """
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square module.
+This module contains a class that defines a square and a method that
+sets its size.
+"""
+
+
+class Square():
+    """create a class."""
+
+    def __init__(self, size):
+        """Set the necessary attributes for the Square object.
+        
+        """
+        self.__size = size
